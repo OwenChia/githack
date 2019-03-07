38d10b8374146bcf7b70881f7ee34383eae93f41
Owen Chia <aptx945@gmail.com>
2019-03-07 19:25:16 +0800

    delete unused code

--- githack/parse/structure.py
+++ githack/parse/structure.py
@@ -8,20 +8,16 @@ class StructureMeta(type):
         byte_order = ''
         offset = 0
         for fieldfmt, fieldname, *func in fields:
-            if isinstance(fieldfmt, StructureMeta):
-                field = NestedStruct(fieldname, fieldfmt, offset)
-                size = fieldfmt.struct_size
-            else:
-                if fieldfmt.startswith(('<', '>', '!', '@')):
-                    byte_order = fieldfmt[0]
-                    fieldfmt = fieldfmt[1:]
-                fieldfmt = byte_order + fieldfmt
+            if fieldfmt.startswith(('<', '>', '!', '@')):
+                byte_order = fieldfmt[0]
+                fieldfmt = fieldfmt[1:]
+            fieldfmt = byte_order + fieldfmt
 
-                field = StructField(fieldfmt, offset)
-                size = struct.calcsize(fieldfmt)
+            field = StructField(fieldfmt, offset)
+            size = struct.calcsize(fieldfmt)
 
-                if len(func) == 1 and callable(func[0]):
-                    field = StructField(fieldfmt, offset, func=func[0])
+            if len(func) == 1 and callable(func[0]):
+                field = StructField(fieldfmt, offset, func=func[0])
 
             setattr(self, fieldname, field)
             offset += size
@@ -42,46 +38,6 @@ class StructField:
             if callable(self.func):
                 r = [self.func(it) for it in r]
             return r[0] if len(r) == 1 else r
-
-
-class NestedStruct:
-    def __init__(self, name, struct_type, offset):
-        self.name = name
-        self.struct_type = struct_type
-        self.offset = offset
-
-    def __get__(self, instance, cls):
-        if instance is None:
-            return self
-        else:
-            data = instance._buffer[self.offset:self.offset+self.struct_type.struct_size]
-            result = self.struct_type(data)
-            setattr(instance, self.name, result)
-            return result
-
-
-class SizedRecord:
-    def __init__(self, bytedata):
-        self._buffer = memoryview(bytedata)
-
-    @classmethod
-    def from_file(cls, fd, size_fmt, includes_size=True):
-        sz_nbytes = struct.calcsize(size_fmt)
-        sz_bytes = fd.read(sz_nbytes)
-        sz, *_ = struct.unpack(size_fmt, sz_bytes)
-        buf = fd.read(sz - includes_size * sz_nbytes)
-        return cls(buf)
-
-    def iter_as(self, code):
-        if isinstance(code, str):
-            s = struct.Struct(code)
-            for off in range(0, len(self._buffer), s.size):
-                yield s.unpack_from(self._buffer, off)
-        elif isinstance(code, StructureMeta):
-            size = code.struct_size
-            for off in range(0, len(self._buffer), size):
-                data = self._buffer[off:off+size]
-                yield code(data)
 
 
 class Structure(metaclass=StructureMeta):
